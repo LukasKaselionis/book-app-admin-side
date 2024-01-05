3fc9a440e20d8bdf8ccdfd9327ff4b07bc3226ce
Lukas Kašelionis <lukas@scoding.com>
2024-01-05 17:14:03 +0200

    initial commit

--- app/Http/Controllers/AuthController.ts
+++ app/Http/Controllers/AuthController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import nodemailer from "nodemailer";
+import bcrypt from "bcrypt";
 import { User } from "../../Models/UserScema";
 
 export default class AuthController {
@@ -7,36 +9,97 @@ export default class AuthController {
     }
 
     public async register(req: Request, res: Response): Promise<void> {
-        try {
-            const { firstName, lastName, email } = req.body;
-            if (!firstName || !lastName || !email) {
-                res.status(400).json({ message: "Missing required fields" });
+        const { firstName, lastName, email } = req.body;
+        if (!firstName || !lastName || !email) {
+            res.status(400).json({ message: "Missing required fields" });
 
-                return;
-            }
-
-            const user = new User({
-                firstName,
-                lastName,
-                email,
-                password: null
-            });
-
-            await user.save();
-
-            res.status(201).json({
-                message: "User created successfully! Check email!"
-            });
-            // TODO: After success user create, need send email were user can set new password
-        } catch (error) {
-            res.status(500).json({
-                message: "An error occurred during registration",
-            });
+            return;
         }
+
+        const user = new User({
+            firstName,
+            lastName,
+            email,
+            password: null,
+        });
+
+        await user
+            .save()
+            .then((user) => {
+                res.status(201).json({
+                    message: "User created successfully! Check email!",
+                });
+                this.sendEmail(email, user._id.toString());
+            })
+            .catch(() => {
+                res.status(500).json({
+                    message: "An error occurred during registration",
+                });
+            });
     }
 
-    public setNewPassword(req: Request, res: Response): void {
-        res.send("password");
+    private async sendEmail(email: string, id: string): Promise<void> {
+        const testAccount = await nodemailer.createTestAccount();
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false,
+            auth: {
+                user: testAccount.user,
+                pass: testAccount.pass,
+            },
+        });
+        const info = await transporter.sendMail({
+            from: "book@app.com",
+            to: email,
+            subject: "Set new password",
+            text: `Click this link and set new password: /api/set-new-password/${id}`,
+        });
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    }
+
+    public async setNewPassword(req: Request, res: Response): Promise<void> {
+        const userId: string = req.params.id;
+        const { password, repeatPassword } = req.body;
+
+        const user = await User.findOne({ _id: userId });
+
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+
+            return;
+        }
+
+        if (password !== repeatPassword) {
+            res.status(400).json({ message: "Passwords do not match" });
+
+            return;
+        }
+
+        user.password = await bcrypt.hash(password, 10);
+        await user.save()
+            .then(() => {
+                res.status(200).json(
+                    {
+                        message: "Password updated successfully",
+                        user: user
+                    }
+                );
+            });
+    }
+
+    public async forgotPassword(req: Request, res: Response): Promise<void> {
+        const { email } = req.body;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+
+            return;
+        } else {
+            res.status(200).json({ message: "Check email and set new password" });
+            await this.sendEmail(email, user._id.toString());
+        }
     }
 
     public logout(req: Request, res: Response): void {
--- router/router.ts
+++ router/router.ts
@@ -6,7 +6,8 @@ const authController: AuthController = new AuthController();
 
 router.post("/login", (req: Request, res: Response) => authController.login(req, res));
 router.post("/register", (req: Request, res: Response) => authController.register(req, res));
-router.post("/set-new-password", (req: Request, res: Response) => authController.setNewPassword(req, res));
+router.put("/set-new-password/:id", (req: Request, res: Response) => authController.setNewPassword(req, res));
+router.post("/forgot-password", (req: Request, res: Response) => authController.forgotPassword(req, res));
 router.get("/logout", (req: Request, res: Response) => authController.logout(req, res));
 
 export default router;

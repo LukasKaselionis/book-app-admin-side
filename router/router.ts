36195fa6de596dfcd4ec72a2d560678be7b4a6ea
Lukas Kašelionis <lukas@scoding.com>
2024-01-05 10:09:09 +0200

    initial commit

--- router/router.ts
+++ router/router.ts
@@ -4,9 +4,9 @@ import AuthController from "../app/Http/Controllers/AuthController";
 const router: Router = express.Router();
 const authController: AuthController = new AuthController();
 
-router.get("/login", (req: Request, res: Response) => authController.login(req, res));
+router.post("/login", (req: Request, res: Response) => authController.login(req, res));
 router.post("/register", (req: Request, res: Response) => authController.register(req, res));
-router.post("/setNewPassword", (req: Request, res: Response) => authController.setNewPassword(req, res));
+router.post("/set-new-password", (req: Request, res: Response) => authController.setNewPassword(req, res));
 router.get("/logout", (req: Request, res: Response) => authController.logout(req, res));
 
 export default router;
